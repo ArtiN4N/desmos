9e5a1ce6c200d35be5c35c396d1d83331aa9acd4
artinan <61893231+ArtiN4N@users.noreply.github.com>
2021-10-13 13:04:02 -0400

    Create desmos.js

---
+++ desmos.js
@@ -0,0 +1,4 @@
+var doc = document.getElementById('calculator');
+var calculator = Desmos.GraphingCalculator(doc);
+calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
+calculator.setExpression({ id: 'graph2', latex: 'y=x^4' });
