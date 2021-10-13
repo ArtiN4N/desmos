41458155ad21ac9fd34d5ab23a130067edcb9436
artinan <61893231+ArtiN4N@users.noreply.github.com>
2021-10-13 13:11:28 -0400

    Update desmos.js

--- desmos.js
+++ desmos.js
@@ -3,4 +3,13 @@ var calculator = Desmos.GraphingCalculator(doc);
 calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
 calculator.setExpression({ id: 'graph2', latex: 'y=x^4' });
 
+let count = 0;
+for (i = 0; i < 20; i++) {
+  for (j = 0; j < 20; j++) {
+    count++;
+    
+    calculator.setExpression({ id: 'graph' + count, latex: 'y=(' + 0.1 * i + ', ' + 0.1 * j + ')' });
+  }
+}
+
 console.log("he");
